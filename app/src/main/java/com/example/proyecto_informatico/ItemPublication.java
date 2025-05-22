fd54649ac15ac8615dee9bdeb3de8a80e36ef9fc
Elpyuo <octaviocesarisidro@gmail.com>
2025-05-22 09:31:28 -0600

    Agregando diseños al proyecto

--- app/src/main/java/com/example/proyecto_informatico/ItemPublication.java
+++ app/src/main/java/com/example/proyecto_informatico/ItemPublication.java
@@ -14,7 +14,7 @@ public class ItemPublication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_item_publicacion);
+        setContentView(R.layout.activity_item_publication);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
