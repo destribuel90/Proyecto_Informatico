52e1a58867c261ea906326959f54f10f13d37e7e
Roli <drer1im1@gmail.com>
2025-06-12 03:46:26 -0600

    edit create

--- app/src/main/java/com/example/proyecto_informatico/Profile.java
+++ app/src/main/java/com/example/proyecto_informatico/Profile.java
@@ -1,6 +1,8 @@
 package com.example.proyecto_informatico;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
 
+    ImageButton btnProfile, btnHome, btnSearch, btnAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        // Configurar el padding con los insets del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Asignar los botones
+        btnProfile = findViewById(R.id.nav_profile);
+        btnHome = findViewById(R.id.nav_home);
+        btnSearch = findViewById(R.id.nav_search);
+        btnAdd = findViewById(R.id.nav_add);
+
+        // Programar los clics
+        btnProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(Profile.this, Profile.class);
+            startActivity(intent);
+        });
+
+        btnHome.setOnClickListener(v -> {
+            Intent intent = new Intent(Profile.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        btnSearch.setOnClickListener(v -> {
+            Intent intent = new Intent(Profile.this, Search.class);
+            startActivity(intent);
+        });
+
+        btnAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(Profile.this, ButtonCreate.class);
+            startActivity(intent);
         });
     }
 }
